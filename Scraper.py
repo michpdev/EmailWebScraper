a88dca9d04430b7affe4137994e562a4468f5cf1
Michelle Prior <michpgamedev@gmail.com>
2022-07-14 22:40:26 -0400

    Testing more

--- Scraper.py
+++ Scraper.py
@@ -13,7 +13,7 @@ quotes=[] # a list to store quotes
 table = soup.find('div', attrs = {'id':'all_quotes'})
 
 for row in table.findAll('div',
-						attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+						attrs = {'class':'col-6 col-lg-4 text-center margin-30px-bottom sm-margin-30px-top'}):
 	quote = {}
 	quote['theme'] = row.h5.text
 	quote['url'] = row.a['href']
