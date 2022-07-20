3b1f86a8bca23a95b7f58c8757f92b9ffbaebab2
Michelle Prior <michpgamedev@gmail.com>
2022-07-20 01:11:51 -0400

    Take in account double last names

--- Scraper.py
+++ Scraper.py
@@ -3,51 +3,41 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL = "https://classes.cornell.edu/browse/roster/FA22/subject/AAS"
+
+URL = "https://classes.cornell.edu/browse/roster/FA22/subject/AMST"
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html.parser')
 print(soup.title)
 
 professors = []
 
+
 class_list = soup.findAll('span', attrs={"class":"tooltip-iws", "data-toggle":"popover"})
 for prof in class_list:
-	if "(" in prof['data-content'] and "Letter" not in prof['data-content']:
+	if "(" in prof['data-content'] and "Letter" not in prof['data-content'] and "Satisfactory" not in prof['data-content']:
 		t = prof['data-content'].split()
 		professor = {}
 		professor['first'] = t[0]
-		professor['last'] = t[1]
-		professor['netid'] = t[2].replace("(","").replace(")","")
+		if (t[2][0].isupper()): 
+			professor['last'] = t[1]+" "+t[2]
+			professor['netid'] = t[3].replace("(","").replace(")","")
+		else:
+			professor['last'] = t[1]
+			professor['netid'] = t[2].replace("(","").replace(")","")
+		
 		professor['email'] = professor['netid']+"@cornell.edu"
 		professors.append(professor)
 
+
+no_dup = []
+for i in professors:
+    if i not in no_dup:
+        no_dup.append(i)
 filename = 'professors.csv'
 
 with open(filename, 'w', newline='') as f:
 	w = csv.DictWriter(f,['first','last','netid','email'])
 	w.writeheader()
-	for profes in professors:
+	for profes in no_dup:
 		w.writerow(profes)		
-
-'''
-professors=[] # a list to store professors
-
-table = soup.find('div', attrs = {"data-roster-slug":"FA22"})
-
-for row in table.findAll('span',
-						attrs = {'class':'tooltips-iws'}):
-	professor = {}
-	professor['first'] = row.text['data-content']
-	professor['last'] = row.a['href']
-	professor['netid'] = row.img['src']
-	professors.append(professor)
-
-filename = 'professors.csv'
-with open(filename, 'w', newline='') as f:
-	w = csv.DictWriter(f,['first','last','netid'])
-	w.writeheader()
-	for professor in professors:
-		w.writerow(professor)
-'''
-
 
